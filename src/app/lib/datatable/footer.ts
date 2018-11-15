5e904737cc2aaaf6418bff4e47daec182f96677e
krishna-agrawal <krishitnitp@gmail.com>
2018-11-15 12:51:03 +0530

    Paginator support for custom footer paginator

--- src/app/lib/datatable/footer.ts
+++ src/app/lib/datatable/footer.ts
@@ -31,7 +31,7 @@ export class Footer {
 
 @Component({
   selector: 'm-globalFooterTemplateLoader',
-  template: ``,
+  template: `<ng-content></ng-content>`,
 })
 export class GlobalFooterTemplateLoader
   implements OnInit, OnChanges, OnDestroy {
@@ -62,11 +62,15 @@ export class GlobalFooterTemplateLoader
   selector: '[mFooter]',
   template: `
     <div *ngIf="footer.template" class="card-footer">
-      <m-globalFooterTemplateLoader [footer]="footer"></m-globalFooterTemplateLoader>
+      <m-globalFooterTemplateLoader [footer]="footer">
+        <div *ngIf="footer.paginator" class="paginator-wrapper">
+          <mat-paginator (page)="dt.pageChange($event)" [length]="(footer.length != undefined) ? footer.length : dt.totalRecords" [pageIndex]="(footer.pageIndex != undefined) ? footer.pageIndex : dt.pageIndex" [pageSize]="footer.pageSize" [pageSizeOptions]="footer.pageSizeOptions"></mat-paginator>
+        </div>
+      </m-globalFooterTemplateLoader>
     </div>
 
     <div *ngIf="!footer.template" class="card-footer">
-      <div *ngIf="footer.paginator">
+      <div *ngIf="footer.paginator" class="paginator-wrapper">
         <mat-paginator (page)="dt.pageChange($event)" [length]="(footer.length != undefined) ? footer.length : dt.totalRecords" [pageIndex]="(footer.pageIndex != undefined) ? footer.pageIndex : dt.pageIndex" [pageSize]="footer.pageSize" [pageSizeOptions]="footer.pageSizeOptions"></mat-paginator>
       </div>
     </div>
@@ -77,6 +81,10 @@ export class GlobalFooterTemplateLoader
       height: var(--card-footer-height, 56px);
       padding: var(--card-footer-padding, 0 14px 0 24px);
       border-top: 1px solid #e0e0e0;
+    }
+    .paginator-wrapper{
+      display: inline-block;
+      float: right;
     }
   `,
   ],

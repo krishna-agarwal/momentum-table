f3b8364e710af8023af07cef2fd77afcb5f842d1
krishna-agrawal <krishitnitp@gmail.com>
2018-11-28 12:34:11 +0530

    column reordering support using drag drop

--- src/app/lib/datatable/column-header.ts
+++ src/app/lib/datatable/column-header.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/core';
 import { DataTable } from './datatable';
 import { ColumnComponent } from './columns';
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'm-columnHeaderTemplateLoader',
@@ -39,13 +40,13 @@ export class ColumnHeaderTemplateLoader implements OnInit, OnDestroy {
 @Component({
   selector: '[mColumnHeader]',
   template: `
-    <tr>
-      <th *ngIf="dt.selectionHandler == true" class="m-checkbox-header">
+    <tr cdkDropList cdkDropListOrientation="horizontal" class="example-list" (cdkDropListDropped)="drop($event)">
+      <th  *ngIf="dt.selectionHandler == true" class="m-checkbox-header">
         <div class="m-header-th m-header-th--checkbox">
           <mat-checkbox [disabled]="dt.selectionMode == 'single'" [checked]="dt.allSelected" (change)="dt.toggleRowsWithCheckbox($event)"></mat-checkbox>
         </div>
       </th>
-      <th [hidden]="col.hidden" *ngFor="let col of columns" (click)="dt.sort($event,col)"
+      <th  cdkDrag cdkDragLockAxis="x" [hidden]="col.hidden" class="example-box" *ngFor="let col of columns" (click)="dt.sort($event,col)"
           [ngClass]="[col.colHeadClass ? col.colHeadClass : '', col.sortable ? 'm-sortable-column': '']">
         <div class="m-header-th">
           <span *ngIf="!col.headerTemplate">{{ col.header }}</span>
@@ -96,6 +97,40 @@ export class ColumnHeaderTemplateLoader implements OnInit, OnDestroy {
       font-size: 12px;
       vertical-align: middle;
     }
+    .example-list {
+      overflow: hidden;
+    }
+
+    .example-box {
+      border-right: solid 1px #ccc;
+    }
+
+    .cdk-drag-preview {
+      box-sizing: border-box;
+      text-align: center;
+      color: rgba(0, 0, 0, 0.54);
+      font-size: 12px;
+      border-radius: 2px;
+      box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
+      0 8px 10px 1px rgba(0, 0, 0, 0.14),
+      0 3px 14px 2px rgba(0, 0, 0, 0.12);
+    }
+
+    .cdk-drag-placeholder {
+      opacity: 0;
+    }
+
+    .cdk-drag-animating {
+      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+    }
+
+    .example-box:last-child {
+      border: none;
+    }
+
+    .example-list.cdk-drop-list-dragging .example-box:not(.cdk-drag-placeholder) {
+      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+    }
   `,
   ],
 })
@@ -105,4 +140,8 @@ export class ColumnHeaderComponent {
     public dt: DataTable,
   ) {}
   @Input('mColumnHeader') columns: ColumnComponent[];
+
+  drop(event: CdkDragDrop<any>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/lib/material.ts
+++ src/app/lib/material.ts
@@ -3,9 +3,10 @@ import {
   MatInputModule, MatListModule, MatPaginatorModule
 } from '@angular/material';
 import {NgModule} from '@angular/core';
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
-  imports: [MatCardModule, MatIconModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatPaginatorModule],
-  exports: [MatCardModule, MatIconModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatPaginatorModule],
+  imports: [MatCardModule, MatIconModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatPaginatorModule, DragDropModule],
+  exports: [MatCardModule, MatIconModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatPaginatorModule, DragDropModule],
 })
 export class MaterialModule { }
